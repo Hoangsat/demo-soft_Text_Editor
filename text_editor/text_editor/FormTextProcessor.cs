109e711902b1ad0db0ba8e9b31c3bc844696bd79
Hoangsat <hoangsat1@gmail.com>
2022-04-23 02:26:50 +0200

    123

---
+++ text_editor/text_editor/FormTextProcessor.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace text_editor
+{
+    public partial class FormTextProcessor : Form
+    {
+        public FormTextProcessor()
+        {
+            InitializeComponent();
+        }
+
+        private void open_file_Click(object sender, EventArgs e)
+        {
+            if (open_fd.ShowDialog() == DialogResult.Cancel)
+            return;
+
+            try
+            { 
+            text_editor.Text = File.ReadAllText(open_fd.FileName, System.Text.Encoding.UTF8);
+  
+            }
+            catch
+            {
+                MessageBox.Show("Error");   
+            }
+
+}
+
+        private void button_save_file_Click(object sender, EventArgs e)
+        {
+            if (save_fd.ShowDialog() == DialogResult.Cancel)
+                return;
+            try
+            {
+                File.WriteAllText(save_fd.FileName, text_editor.Text, System.Text.Encoding.UTF8);
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void button_replace_Click(object sender, EventArgs e)
+        {
+
+            if (text_pattern.Text == "")
+                return;
+            
+            text_editor.Text = text_editor.Text.Replace(text_pattern.Text, text_replace.Text);
+        }
+
+     
+        
+    }
+}
